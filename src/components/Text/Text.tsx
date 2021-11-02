a8d17485527c7e790d0c2b08a33be42bccfa5903
Ewelina Skibinska <E.Skibinska@wellcome.ac.uk>
2021-11-02 11:24:19 +0000

    9135 task: changes InfoBox title to

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -7,6 +7,7 @@ type TextProps = {
   children: string;
   className?: string;
   title?: string;
+  titleAs?: 'h3' | 'p';
   variant?: 'text' | 'text-snippet';
 };
 
@@ -14,15 +15,19 @@ export const Text = ({
   children,
   className,
   title,
+  titleAs = 'h3',
   variant = 'text'
 }: TextProps) => {
+  const TitleElement = variant === 'text-snippet' ? 'p' : titleAs;
   const classNames = cx(`cc-${variant}`, {
     [className]: className
   });
 
   return (
     <div className={classNames}>
-      {title && <h3 className={`cc-${variant}__title`}>{title}</h3>}
+      {title && (
+        <TitleElement className={`cc-${variant}__title`}>{title}</TitleElement>
+      )}
       <RichText variant={variant}>{children}</RichText>
     </div>
   );
