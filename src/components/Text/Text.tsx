62a7d63ddf3b5f9bf84b80c6f0b35c78f7702f37
Ewelina Skibinska <E.Skibinska@wellcome.ac.uk>
2021-11-03 16:36:43 +0000

    9135 task(refactor): title is only avaiable for text-snippet variant

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -7,7 +7,6 @@ type TextProps = {
   children: string;
   className?: string;
   title?: string;
-  titleAs?: 'h3' | 'p';
   variant?: 'text' | 'text-snippet';
 };
 
@@ -15,19 +14,15 @@ export const Text = ({
   children,
   className,
   title,
-  titleAs = 'h3',
   variant = 'text'
 }: TextProps) => {
-  const TitleElement = variant === 'text-snippet' ? 'p' : titleAs;
   const classNames = cx(`cc-${variant}`, {
     [className]: className
   });
 
   return (
     <div className={classNames}>
-      {title && (
-        <TitleElement className={`cc-${variant}__title`}>{title}</TitleElement>
-      )}
+      {title && <p className={`cc-${variant}__title`}>{title}</p>}
       <RichText variant={variant}>{children}</RichText>
     </div>
   );
