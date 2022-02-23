df85dc19efb3e47ea33f6a48cf538f42cb383db6
Ewelina Skibinska <E.Skibinska@wellcome.ac.uk>
2022-02-23 16:40:57 +0000

    9592 task(chore): removes SidebarSearch

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,6 @@ export { RouterLinkWrapper } from 'RouterLinkWrapper/RouterLinkWrapper';
 export { SearchForm } from 'SearchForm/SearchForm';
 export { Section } from 'Section/Section';
 export { SectionTitle } from 'SectionTitle/SectionTitle';
-export { default as SidebarSearch } from 'SidebarSearch';
 export { TableauChart } from 'TableauChart/TableauChart';
 export { default as TagList } from 'TagList';
 export { Text } from 'Text/Text';
