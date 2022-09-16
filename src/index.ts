146f7c0f72fdd2e0b8906359facc1aebf591cf2f
Ewelina Skibinska <E.Skibinska@wellcome.ac.uk>
2022-09-16 09:45:35 +0200

    9757 task: removes SectionTitle

--- src/index.ts
+++ src/index.ts
@@ -13,4 +13,3 @@ export { default as Link } from 'Link';
 export { Logo } from 'Logo/Logo';
 export { RouterLinkWrapper } from 'RouterLinkWrapper/RouterLinkWrapper';
 export { Section } from 'Section/Section';
-export { SectionTitle } from 'SectionTitle/SectionTitle';
