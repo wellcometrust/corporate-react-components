ab5243d124858a4136e7b2f840d8642c0374984f
Ewelina Skibinska <E.Skibinska@wellcome.ac.uk>
2022-09-20 14:52:59 +0100

    9746 task: removes Grid and GridCell

--- src/components/Grid/index.ts
+++
@@ -1,1 +0,0 @@
-export { default, GridCell } from './Grid';
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import 'assets/styles/app.scss';
 // standard components
 export { Button } from 'Button/Button';
 export { default as FormField } from 'FormField';
-export { Grid, GridCell } from 'Grid/Grid';
 export { Icon } from 'Icon/Icon';
 export { default as Label } from 'Label';
 export { default as Link } from 'Link';
